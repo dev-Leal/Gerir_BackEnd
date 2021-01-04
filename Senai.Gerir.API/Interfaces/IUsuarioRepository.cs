4168c480e3ca29881141444fe6a1fc7bf029079d
Beatriz Oliveira Leal <43407489862@info.local>
2021-01-04 17:38:06 -0300

    Api iniciada e Database finalizada

---
+++ Senai.Gerir.API/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Senai.Gerir.API.Dominios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Gerir.API.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        Usuario Cadastrar(Usuario usuario);
+        Usuario Logar(String email, String senha);
+        Usuario Editar(Usuario usuario);
+        void Remover(Guid Id);
+        Usuario BuscarPorId(Guid Id);
+
+
+    }
+}
